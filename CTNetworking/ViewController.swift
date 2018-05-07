a84e62418f3902079b37bb4723e6fc3ad60db8e6
wangjie <17054214@qq.com>
2018-05-07 22:59:43 +0800

    init

---
+++ CTNetworking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CTNetworking
+//
+//  Created by 王杰 on 2018/5/7.
+//  Copyright © 2018年 王杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
